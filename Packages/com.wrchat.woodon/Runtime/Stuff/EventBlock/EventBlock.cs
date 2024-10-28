ed9c563bd0adc0711806f28f11190a19e218b586
카모뜨린・Mascari4615 <mascari4615@gmail.com>
2024-10-28 11:46:59 +0900

    EventBlock 개선
    - 주석 추가
    - 초기 예외처리
    - OnwerObject 추가 (OwnerObject의 오너일 경우에만 이벤트 발생 조건)

--- Packages/com.wrchat.woodon/Runtime/Stuff/EventBlock/EventBlock.cs
+++ Packages/com.wrchat.woodon/Runtime/Stuff/EventBlock/EventBlock.cs
@@ -8,20 +8,66 @@ namespace WRC.Woodon
 	public class EventBlock : MBase
 	{
 		[Header("_" + nameof(EventBlock))]
+		
+		[Tooltip("이벤트를 발생시킬 Udon들입니다.")]
 		[SerializeField] private UdonSharpBehaviour[] targetUdons = new UdonSharpBehaviour[0];
+		
+		[Tooltip("targetUdons와 같은 인덱스에 있는 Udon에게 호출할 메소드 이름입니다.")]
 		[SerializeField] private string[] methodNames = new string[0];
 
+		[Header("_" + nameof(EventBlock) + " - Options")]
+
+		[Tooltip("만약 비어있지 않다면, OwnerObject의 오너일 경우에만 이벤트를 발생시킵니다.")]
+		[SerializeField] private UdonSharpBehaviour ownerObject = null;
+
+		private void Start()
+		{
+			Init();
+		}
+
+		private void Init()
+		{
+			MDebugLog($"{nameof(Init)}");
+
+			if (targetUdons.Length != methodNames.Length)
+			{
+				MDebugLog($"{nameof(targetUdons)}.Length != {nameof(methodNames)}.Length", LogType.Error);
+				return;
+			}
+
+			for (int i = 0; i < targetUdons.Length; i++)
+			{
+				if (targetUdons[i] == null)
+				{
+					MDebugLog($"{nameof(targetUdons)}[{i}] == null", LogType.Error);
+					return;
+				}
+
+				if (string.IsNullOrEmpty(methodNames[i]))
+				{
+					MDebugLog($"{nameof(methodNames)}[{i}] == null", LogType.Warning);
+					return;
+				}
+			}
+		}
+
+		[ContextMenu(nameof(Invoke))]
 		public void Invoke()
 		{
 			MDebugLog($"{nameof(Invoke)}");
+
+			if ((ownerObject != null) && (IsOwner(ownerObject.gameObject) == false))
+				return;
 
 			for (int i = 0; i < targetUdons.Length; i++)
 				targetUdons[i].SendCustomEvent(methodNames[i]);
 		}
 
+		[ContextMenu(nameof(Invoke_G))]
 		public void Invoke_G()
 		{
 			MDebugLog($"{nameof(Invoke_G)}");
+
 			SendCustomNetworkEvent(NetworkEventTarget.All, nameof(Invoke));
 		}
 	}
