839771655e5e019138ae9a2ac4ce678d08ae8110
KarmoDDrine <mascari4615@gmail.com>
2024-11-17 15:23:24 +0900

    LookAtPlayer isForUI 옵션 추가

--- Packages/com.wrchat.woodon/Runtime/Component/Transform/LookAtPlayer/LookAtPlayer.cs
+++ Packages/com.wrchat.woodon/Runtime/Component/Transform/LookAtPlayer/LookAtPlayer.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿using UdonSharp;
+using UnityEngine;
 using VRC.SDKBase;
 using static WRC.Woodon.MUtil;
 
 namespace WRC.Woodon
 {
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 	public class LookAtPlayer : MBase
 	{
+		[Header("_" + nameof(LookAtPlayer))]
 		[SerializeField] private HumanBodyBones targetBone = HumanBodyBones.Head;
 		[SerializeField] private MTarget mTarget;
 		[SerializeField] private bool lerp;
 		[SerializeField] private float lerpValue = 5;
+		[SerializeField] private bool isForUI = false;
 
 		private void Update()
 		{
@@ -28,10 +32,17 @@ namespace WRC.Woodon
 			Vector3 headPos = targetPlayer.GetBonePosition(targetBone);
 
 			if (lerp)
+			{
 				transform.rotation = Quaternion.Lerp(transform.rotation,
 					Quaternion.LookRotation(headPos - transform.position), Time.deltaTime * lerpValue);
+			}
 			else
+			{
 				transform.LookAt(headPos);
+			}
+
+			if (isForUI)
+				transform.Rotate(0, 180, 0);
 		}
 	}
 }
