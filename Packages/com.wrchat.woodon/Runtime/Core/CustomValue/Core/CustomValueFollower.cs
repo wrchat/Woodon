46c34cda865ada9e07b2d420beb2461d2504dc25
KarmoDDrine <mascari4615@gmail.com>
2025-02-06 04:10:18 +0900

    CustomValue* abstract 클래스들 주석처리
    직접 사용하지 않을 뿐더러, 경고 로그만 찍어내서 클래스 전체 주석 처리

--- Packages/com.wrchat.woodon/Runtime/Core/CustomValue/Core/CustomValueFollower.cs
+++ Packages/com.wrchat.woodon/Runtime/Core/CustomValue/Core/CustomValueFollower.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 namespace WRC.Woodon
 {
-	public abstract class CustomValueFollower<T> : MBase
-	{
-		[Header("_" + nameof(CustomValueFollower<T>))]
-		[SerializeField] protected T customValue;
+	// public abstract class CustomValueFollower<T> : MBase
+	// {
+	// 	[Header("_" + nameof(CustomValueFollower<T>))]
+	// 	[SerializeField] protected T customValue;
 
-		public virtual void SetCustomValue(T customValue)
-		{
-			// MDebugLog($"{nameof(SetCustomValue)} - {customValue}");
-			this.customValue = customValue;
-		}
-	}
+	// 	public virtual void SetCustomValue(T customValue)
+	// 	{
+	// 		// MDebugLog($"{nameof(SetCustomValue)} - {customValue}");
+	// 		this.customValue = customValue;
+	// 	}
+	// }
 }
