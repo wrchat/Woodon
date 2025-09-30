c2bec9fc5a9aa607e3cd240844aa61dbbefa691d
KarmoDDrine <mascari4615@gmail.com>
2025-09-30 23:48:17 +0900

    [2025-09-30] Pos Info 기능

---
+++ Packages/com.wrchat.woodon/Runtime/Component/UI/PosInfo/PosInfo.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+
+namespace WRC.Woodon
+{
+	// 현재 위치 정보를 잠깐 표시해주는 UI (포켓몬스터 202번 도로 진입하면 텍스트 뜨는 것처럼)
+	// KarmoDDrine - 2025-09-30. 23:20
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class PosInfo : WBase
+	{
+		[SerializeField] private TextMeshProUGUI posText;
+		[SerializeField] private WAnimator animator;
+
+		public void ShowPosInfo(string posName)
+		{
+			if (posText != null)
+				posText.text = posName;
+
+			if (animator != null)
+				animator.SetTrigger_L0();
+		}
+	}
+}
---
+++ Packages/com.wrchat.woodon/Runtime/Component/UI/PosInfo/PosInfoTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace WRC.Woodon
+{
+	// PosInfo 트리거 (영역에 들어가면 PosInfo에 위치 이름을 보내는 역할)
+	// KarmoDDrine - 2025-09-30. 23:25
+	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class PosInfoTrigger : WBase
+	{
+		[Header("_" + nameof(PosInfoTrigger))]
+		[SerializeField] private PosInfo posInfo;
+		[SerializeField] private string posName;
+
+		public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+		{
+			WDebugLog($"{nameof(OnPlayerTriggerEnter)} : {player.displayName} | {player.playerId}");
+
+			bool isLocalPlayer = player == Networking.LocalPlayer;
+
+			if (isLocalPlayer == false)
+				return;
+
+			posInfo.ShowPosInfo(posName);
+		}
+	}
+}
