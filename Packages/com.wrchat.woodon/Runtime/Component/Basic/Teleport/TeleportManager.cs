7c1ea6009ede829f55639da18a21d83d9e497e9c
KarmoDDrine <mascari4615@gmail.com>
2025-09-30 23:48:44 +0900

    [2025-09-30] TeleportManager, ElevatorButtons (View)

---
+++ Packages/com.wrchat.woodon/Runtime/Component/Basic/Teleport/TeleportManager.cs
@@ -0,0 +1,55 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace WRC.Woodon
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class TeleportManager : WBase
+	{
+		[Header("_" + nameof(TeleportManager))]
+		[SerializeField] private Transform[] targetPoses;
+		[SerializeField] private WSFXManager sfxManager;
+		[SerializeField] private AudioClip sfxClip;
+
+		public void Teleport(int index)
+		{
+			WDebugLog($"{nameof(Teleport)} called with index {index}");
+
+			if (index < 0 || index >= targetPoses.Length)
+			{
+				WDebugLog($"Index {index} is out of bounds (0-{targetPoses.Length - 1})", LogType.Error);
+				return;
+			}
+
+			Networking.LocalPlayer.TeleportTo(targetPoses[index].position, targetPoses[index].rotation);
+
+			if (sfxManager != null && sfxClip != null)
+				sfxManager.PlaySFX(sfxClip);
+		}
+
+		#region Horrible UdonEvents
+		public void Teleport0() => Teleport(0);
+		public void Teleport1() => Teleport(1);
+		public void Teleport2() => Teleport(2);
+		public void Teleport3() => Teleport(3);
+		public void Teleport4() => Teleport(4);
+		public void Teleport5() => Teleport(5);
+		public void Teleport6() => Teleport(6);
+		public void Teleport7() => Teleport(7);
+		public void Teleport8() => Teleport(8);
+		public void Teleport9() => Teleport(9);
+		public void Teleport10() => Teleport(10);
+		public void Teleport11() => Teleport(11);
+		public void Teleport12() => Teleport(12);
+		public void Teleport13() => Teleport(13);
+		public void Teleport14() => Teleport(14);
+		public void Teleport15() => Teleport(15);
+		public void Teleport16() => Teleport(16);
+		public void Teleport17() => Teleport(17);
+		public void Teleport18() => Teleport(18);
+		public void Teleport19() => Teleport(19);
+		public void Teleport20() => Teleport(20);
+		#endregion
+	}
+}
