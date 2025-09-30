41762ac04ff0d080a0a44963928eabb3ae5b0422
KarmoDDrine <mascari4615@gmail.com>
2025-09-30 23:49:01 +0900

    [2025-09-30] SkyboxTransition 정리

--- Packages/com.wrchat.woodon/Runtime/Component/Basic/SkyboxTransition/SkyboxTransition.cs
+++ Packages/com.wrchat.woodon/Runtime/Component/Basic/SkyboxTransition/SkyboxTransition.cs
@@ -7,38 +7,42 @@ using VRC.SDKBase;
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class SkyboxTransition : UdonSharpBehaviour
 {
-    [SerializeField] private Material[] skyBoxes;
+	[SerializeField] private Material[] skyBoxes;
 
-    [UdonSynced, FieldChangeCallback(nameof(State))]
-    private int state = 0;
+	[UdonSynced, FieldChangeCallback(nameof(State))] private int state = 0;
+	public int State
+	{
+		get => state;
+		set
+		{
+			state = value;
+			OnStateChange();
+		}
+	}
 
-    public int State
-    {
-        get => state;
-        set
-        {
-            state = value;
-            OnStateChange();
-        }
-    }
+	private void Start()
+	{
+		OnStateChange();
+	}
 
-    private void Start()
-    {
-        OnStateChange();
-    }
+	private void OnStateChange()
+	{
+		RenderSettings.skybox = skyBoxes[state];
+	}
 
-    private void OnStateChange()
-    {
-        RenderSettings.skybox = skyBoxes[state];
-    }
+	public override void Interact()
+	{
+		NextSkybox();
+	}
+	
+	[ContextMenu(nameof(NextSkybox))]
+	public void NextSkybox()
+	{
+		if (Networking.IsOwner(gameObject) == false)
+			Networking.SetOwner(Networking.LocalPlayer, gameObject);
 
-    public override void Interact()
-    {
-        if (!Networking.IsOwner(gameObject))
-            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+		State = (State + 1) % skyBoxes.Length;
 
-        State = (State + 1) % skyBoxes.Length;
-
-        RequestSerialization();
-    }
+		RequestSerialization();
+	}
 }
